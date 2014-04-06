65ca68f7014ba05385729cd92b20756906d43fcc
nathanspeller <speller.nathan@gmail.com>
2014-04-06 12:43:55 -0700

    init ios project

---
+++ Clipster/Clipster/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Clipster
+//
+//  Created by Nathan Speller on 4/6/14.
+//  Copyright (c) 2014 Nathan Speller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Clipster/Clipster/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Clipster
+//
+//  Created by Nathan Speller on 4/6/14.
+//  Copyright (c) 2014 Nathan Speller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
