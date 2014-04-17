ade4c3cda21f324922aaa6bc6a96c57c930a2ccc
Anthony Sherbondy <ts1235813@gmail.com>
2014-04-17 15:51:33 -0700

    move all google search to YouTubeVideo class

--- Clipster/YouTubeVideo.h
+++ Clipster/YouTubeVideo.h
@@ -8,10 +8,14 @@
 
 #import <Foundation/Foundation.h>
 
+#import <GTLServiceYouTube.h>
+
 @interface YouTubeVideo : NSObject
 @property (nonatomic, strong) NSString *title;
 @property (nonatomic, strong) NSString *thumbnailURL;
 @property (nonatomic, strong) NSString *videoId;
 
 + (NSArray *)videosFromSearchResults:(NSArray *)searchResults;
++ (GTLServiceYouTube *)youTubeService;
++ (void)searchVideosWithQuery:(NSString *)queryString completionHandler:(void(^)(NSArray *videos, NSError *error))completionHandler;
 @end
--- Clipster/YouTubeVideo.m
+++ Clipster/YouTubeVideo.m
@@ -13,6 +13,10 @@
 #import <GTLYouTubeThumbnail.h>
 #import <GTLYouTubeResourceId.h>
 
+// Query
+#import <GTLQueryYouTube.h>
+#import <GTLYouTubeSearchListResponse.h>
+
 @implementation YouTubeVideo
 
 + (NSArray *)videosFromSearchResults:(NSArray *)searchResults
@@ -33,4 +37,49 @@
     }
     return videos;
 }
+
+static NSString *const kAPIKey = @"AIzaSyC2068T7T8YpkzNsHK-Cx5kMVJ7f-ZNhOw";
++ (GTLServiceYouTube *)youTubeService
+{
+    static dispatch_once_t once;
+    static GTLServiceYouTube *service;
+    dispatch_once(&once, ^{
+        service = [[GTLServiceYouTube alloc] init];
+        service.APIKey = kAPIKey;
+    });
+    return service;
+}
+
++ (void)searchVideosWithQuery:(NSString *)queryString completionHandler:(void (^)(NSArray *, NSError *))completionHandler
+{
+    GTLServiceYouTube *service = [YouTubeVideo youTubeService];
+    
+    GTLQueryYouTube *query = [GTLQueryYouTube queryForSearchListWithPart:@"snippet,id"];
+    query.q = queryString;
+    query.type = @"video";
+    
+    // maxResults specifies the number of results per page.  Since we earlier
+    // specified shouldFetchNextPages=YES, all results should be fetched,
+    // though specifying a larger maxResults will reduce the number of fetches
+    // needed to retrieve all pages.
+    query.maxResults = 10;
+    
+    // We can specify the fields we want here to reduce the network
+    // bandwidth and memory needed for the fetched collection.
+    //
+    // For example, leave query.fields as nil during development.
+    // When ready to test and optimize your app, specify just the fields needed.
+    // For example, this sample app might use
+    //
+    // query.fields = @"kind,etag,items(id,etag,kind,contentDetails)";
+    
+    [service executeQuery:query completionHandler:^(GTLServiceTicket *ticket, GTLYouTubeSearchListResponse *response, NSError *error) {
+        NSArray *videos = error ? nil : [YouTubeVideo videosFromSearchResults:response.items];
+        completionHandler(videos, error);
+    }];
+    
+}
+
+
+
 @end
