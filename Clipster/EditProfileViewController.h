e7897196d4a2f5ea26b5eff9943fc3c10c3f1b95
Anthony Sherbondy <ts1235813@gmail.com>
2014-04-21 15:25:49 -0700

    can link account to facebook and grab profile image

--- Clipster/EditProfileViewController.h
+++ Clipster/EditProfileViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface EditProfileViewController : UIViewController
+@interface EditProfileViewController : UIViewController <NSURLConnectionDelegate>
 
 @end
--- Clipster/EditProfileViewController.m
+++ Clipster/EditProfileViewController.m
@@ -9,8 +9,11 @@
 #import "EditProfileViewController.h"
 #import "User.h"
 
+#import <Parse/Parse.h>
+
 @interface EditProfileViewController ()
 @property (weak, nonatomic) IBOutlet PFImageView *profileThumbnailView;
+@property (nonatomic, strong) NSMutableData *imageData;
 @end
 
 @implementation EditProfileViewController
@@ -37,8 +40,62 @@
     self.profileThumbnailView.layer.masksToBounds = YES;
 }
 
+- (void)updateProfileThumbnailWithFacebook
+{
+    // Create request for user's Facebook data
+    FBRequest *request = [FBRequest requestForMe];
+
+    // Send request to Facebook
+    [request startWithCompletionHandler:^(FBRequestConnection *connection, id result, NSError *error) {
+        if (!error) {
+            // result is a dictionary with the user's Facebook data
+            NSDictionary *userData = (NSDictionary *)result;
+            NSString *facebookID = userData[@"id"];
+            
+            // URL should point to https://graph.facebook.com/{facebookId}/picture?type=large&return_ssl_resources=1
+            NSURL *pictureURL = [NSURL URLWithString:[NSString stringWithFormat:@"https://graph.facebook.com/%@/picture?type=large&return_ssl_resources=1", facebookID]];
+            
+            // Download the user's facebook profile picture
+            _imageData = [[NSMutableData alloc] init]; // the data will be loaded in here
+            NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:pictureURL
+                                                                      cachePolicy:NSURLRequestUseProtocolCachePolicy
+                                                                  timeoutInterval:2.0f];
+            // Run network request asynchronously
+            NSURLConnection *connection = [[NSURLConnection alloc] initWithRequest:urlRequest delegate:self];
+            
+        }
+    }];
+}
+
+// Called every time a chunk of the data is received
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+    [self.imageData appendData:data]; // Build the image
+}
+
+// Called when the entire image is finished downloading
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+    // Set the thumbnail image for current user and save
+    User *user = [User currentUser];
+    user.thumbnail = [PFFile fileWithData:self.imageData];
+    [user saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        [self refreshUI];
+    }];
+}
+
 - (IBAction)linkWithFacebookClicked:(id)sender
 {
+    User *user = [User currentUser];
+    if (![PFFacebookUtils isLinkedWithUser:user]) {
+        [PFFacebookUtils linkUser:user permissions:nil block:^(BOOL succeeded, NSError *error) {
+            if (succeeded) {
+                // facebook link succeeded
+                [self updateProfileThumbnailWithFacebook];
+            }
+        }];
+    } else {
+        // already linked user
+        [self updateProfileThumbnailWithFacebook];
+    }
 }
 
 @end
